f7c245e9c67911c2f6e4309ce6b5dd7f710d0829
fhl <2105642835@qq.com>
2025-02-07 17:17:51 +0800

    fix: 统一资源文件路径配置，修复 404 问题

--- vite.config.js
+++ vite.config.js
@@ -16,16 +16,19 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       output: {
-        chunkFileNames: 'assets/js/[name].[hash].js',
-        entryFileNames: 'assets/js/[name].[hash].js',
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
         assetFileNames: ({name}) => {
           if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-            return 'assets/images/[name].[hash][extname]'
+            return 'assets/images/[name]-[hash][extname]'
           }
           if (/\.css$/.test(name ?? '')) {
-            return 'assets/css/[name].[hash][extname]'
+            return 'assets/css/[name]-[hash][extname]'
           }
-          return 'assets/[name].[hash][extname]'
+          if (/\.js$/.test(name ?? '')) {
+            return 'assets/js/[name]-[hash][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
         },
         manualChunks: {
           'vendor': ['vue', 'vue-router', 'pinia'],
@@ -33,8 +36,7 @@ export default defineConfig({
         }
       }
     },
-    minify: 'esbuild',
-    cssCodeSplit: true
+    minify: 'esbuild'
   },
   server: {
     port: 8889,
